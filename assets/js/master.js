f176886882f640634544791e91571d5866f61d7c
usmileapp <108572045+it-usmile@users.noreply.github.com>
2024-11-16 10:03:50 +0700

    Update master.js

--- assets/js/master.js
+++ assets/js/master.js
@@ -54,12 +54,11 @@ function urlParams() {
   return result;
 }
 
-function setCookie(cname, cvalue, exdays = 1, path = "") {
-  path = "/" + path;
+function setCookie(cname, cvalue, exdays = 1) {
   const d = new Date();
   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
   let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + `; path=/${path}; ` + expires;
+  document.cookie = cname + "=" + cvalue + `; path=/; ` + expires;
 }
 
 if (currentTheme) {
